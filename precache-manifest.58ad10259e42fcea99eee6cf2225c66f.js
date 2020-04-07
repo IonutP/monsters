de01350be34d46de92d940a648211101d4b54960
Ionut Panait <pacalici@gmail.com>
2020-04-07 16:54:00 +0300

    Updates

---
+++ precache-manifest.58ad10259e42fcea99eee6cf2225c66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43600f733d59a00cbf83c91ae64e09d5",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b212780d2ee1763696b8",
+    "url": "/monsters/static/css/main.6d1ddfaa.chunk.css"
+  },
+  {
+    "revision": "696cd664e94359881443",
+    "url": "/monsters/static/js/2.259342ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.259342ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b212780d2ee1763696b8",
+    "url": "/monsters/static/js/main.94ffc5fa.chunk.js"
+  },
+  {
+    "revision": "326430f4b7509fab48ac",
+    "url": "/monsters/static/js/runtime-main.65b4cb25.js"
+  }
+]);
